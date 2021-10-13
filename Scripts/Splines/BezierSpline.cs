3fd05d6a22c83122a586ef09e7f77374190fd9a9
Daniel Tranca <danieltrancax@gmail.com>
2021-10-13 21:03:56 +0100

    feat: added bezier spline

--- Scripts/Splines/BezierSpline.cs
+++ Scripts/Splines/BezierSpline.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -13,13 +14,13 @@ namespace FriedSynapse.FlowEnt
         public BezierSpline(params Vector3[] points) : base(points)
         {
         }
-
-        private Vector3 startPoint;
+        Vector3[] smoothPoints;
+        private int segmentCount;
+        private int segment = -1;
+        private int segmentPlusOne;
         private Vector3 startControl;
         private Vector3 endControl;
         private Vector3 endPoint;
-        private int segmentCount;
-        private float segmentLength;
 
         protected override void Init()
         {
@@ -38,53 +39,36 @@ namespace FriedSynapse.FlowEnt
                 return;
             }
 
-            segmentCount = points.Length - 4;
-            segmentLength = 1f / segmentCount;
+            segmentCount = points.Length - 1;
 
             int count = points.Length;
-            Vector3[] pointsCache = new Vector3[count];
-            pointsCache[0] = points[0];
-            pointsCache[count - 1] = points[count - 1];
+            smoothPoints = new Vector3[count];
+            smoothPoints[0] = points[0];
+            smoothPoints[count - 1] = points[count - 1];
             for (int i = 1; i < count - 1; i++)
             {
-                pointsCache[i] = (points[i - 1] / 6f) + (points[i] * twoThirds) + (points[i + 1] / 6f);
+                smoothPoints[i] = (points[i - 1] / 6f) + (points[i] * twoThirds) + (points[i + 1] / 6f);
             }
-            points = pointsCache;
         }
-
 
         public override Vector3 GetPoint(float t)
         {
-            //if there are no segments we simply have a bezier case
-            if (segmentCount == 0)
+            float segmentedT = t * segmentCount;
+            int currentSegment = Mathf.FloorToInt(segmentedT);
+            if (currentSegment != segment)
             {
-                startPoint = points[0];
-                startControl = points[1];
-                endControl = points[2];
-                endPoint = points[3];
+                segment = currentSegment;
+                segmentPlusOne = Math.Min(segment + 1, segmentCount);
+
+                startControl = (points[segment] * twoThirds) + (points[segmentPlusOne] / 3f);
+                endControl = (points[segment] / 3f) + (points[segmentPlusOne] * twoThirds);
+                endPoint = smoothPoints[segmentPlusOne];
             }
-            //otherwise we have to lerp through the points and apply bezier for every segment
-            else
-            {
-                int segment = (int)(t / segmentLength);
 
-                if (segment >= segmentCount)
-                {
-                    segment = segmentCount - 1;
-                }
+            float segmentT = segmentedT - segment;
 
-                float segmentT = (t - (segmentLength * segment)) / segmentLength;
-
-                //Debug.Log($"{t} - {segment}, {segmentT}");
-
-                startPoint = Vector3.LerpUnclamped(points[segment], points[segment + 1], segmentT);
-                startControl = Vector3.LerpUnclamped(points[segment + 1], points[segment + 2], segmentT);
-                endControl = Vector3.LerpUnclamped(points[segment + 2], points[segment + 3], segmentT);
-                endPoint = Vector3.LerpUnclamped(points[segment + 3], points[segment + 4], segmentT);
-            }
-            return GetPoint(t, startPoint, startControl, endControl, endPoint);
+            return GetPoint(segmentT, smoothPoints[segment], startControl, endControl, endPoint);
         }
-
 
         public static Vector3 GetPoint(float t, Vector3 startPoint, Vector3 startControl, Vector3 endControl, Vector3 endPoint)
         {
