443119d7f1193c584c66192f23ddb2d0da689711
Chris <hargrove.chris91@gmail.com>
2021-11-24 13:41:29 +0000

    docs: added clamping remarks

--- Scripts/Extensions/Engine/AudioMotionExtensions.cs
+++ Scripts/Extensions/Engine/AudioMotionExtensions.cs
@@ -38,6 +38,7 @@ namespace FriedSynapse.FlowEnt
         /// <summary>
         /// Applies a <see cref="VolumeMotion" /> to the tween.
         /// </summary>
+        /// <remarks>Calculated destination value (current value + <paramref name="value"/>) will be clamped between 0 and 1.</remarks>
         /// <param name="motion"></param>
         /// <param name="value"></param>
         public static TweenMotion<AudioSource> Volume(this TweenMotion<AudioSource> motion, float value)
@@ -46,6 +47,7 @@ namespace FriedSynapse.FlowEnt
         /// <summary>
         /// Applies a <see cref="VolumeToMotion" /> to the tween.
         /// </summary>
+        /// <remarks><paramref name="to"/> value will be clamped between 0 and 1.</remarks>
         /// <param name="motion"></param>
         /// <param name="to"></param>
         public static TweenMotion<AudioSource> VolumeTo(this TweenMotion<AudioSource> motion, float to)
@@ -54,7 +56,9 @@ namespace FriedSynapse.FlowEnt
         /// <summary>
         /// Applies a <see cref="VolumeToMotion" /> to the tween.
         /// </summary>
+        /// /// <remarks><paramref name="from"/> and <paramref name="to"/> values will be clamped between 0 and 1.</remarks>
         /// <param name="motion"></param>
+        /// <param name="from"></param>
         /// <param name="to"></param>
         public static TweenMotion<AudioSource> VolumeTo(this TweenMotion<AudioSource> motion, float from, float to)
             => motion.Apply(new VolumeToMotion(motion.Item, from, to));
