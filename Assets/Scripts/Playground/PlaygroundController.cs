9228fc09df27be7c071ec31562ca949d2f1ec129
Daniel Tranca <danieltrancax@gmail.com>
2021-12-28 21:35:45 +0000

    chore: cleaned playground

--- Assets/Scripts/Playground/PlaygroundController.cs
+++ Assets/Scripts/Playground/PlaygroundController.cs
@@ -9,19 +9,11 @@ namespace FriedSynapse.FlowEnt.Builder
         [SerializeField]
         private List<Transform> objects;
         private List<Transform> Objects => objects;
-
-        [SerializeField]
-        private TweenBuilder tweenBuilder;
-
-        [SerializeField]
-        private ParticleSystem particles;
-        private ParticleSystem Particles => particles;
 #pragma warning restore RCS1169, IDE0044
 
 #pragma warning disable IDE0051, RCS1213
         private void Start()
         {
-            Particles.Echo().SetDelay(2).ConvergeTo(Objects[1], 10f).Start();
         }
 
         private void OnDrawGizmos()
