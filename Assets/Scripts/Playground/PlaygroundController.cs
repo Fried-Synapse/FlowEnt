53e2d6de6727b53f568497d2fe9e5a514898e0b4
Daniel Tranca <danieltrancax@gmail.com>
2022-01-05 22:53:35 +0000

    chore: playground update

--- Assets/Scripts/Playground/PlaygroundController.cs
+++ Assets/Scripts/Playground/PlaygroundController.cs
@@ -1,5 +1,3 @@
-using System.Collections.Generic;
-using FriedSynapse.FlowEnt.Motions.Echo;
 using UnityEngine;
 
 namespace FriedSynapse.FlowEnt.Builder
@@ -8,17 +6,25 @@ namespace FriedSynapse.FlowEnt.Builder
     {
 #pragma warning disable RCS1169, IDE0044
         [SerializeField]
-        private List<Transform> objects;
-        private List<Transform> Objects => objects;
+        private Transform character;
+        private Transform Character => character;
+
+        [SerializeField]
+        private Transform green;
+        private Transform Green => green;
+
+        [SerializeField]
+        private Transform yellow;
+        private Transform Yellow => yellow;
+
+        [SerializeField]
+        private Transform red;
+        private Transform Red => red;
 #pragma warning restore RCS1169, IDE0044
 
 #pragma warning disable IDE0051, RCS1213
         private void Start()
         {
-            Objects[0].Echo()
-                .MoveTo(Objects[1], 5, SpeedType.Gravity)
-                .LookAt(Objects[1])
-                .Start();
         }
 
         private void OnDrawGizmos()
