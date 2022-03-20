9f55bd40ad4a54491bc3d26e2717b6b64be2489e
Daniel Tranca <danieltrancax@gmail.com>
2022-03-20 23:31:46 +0000

    chore: temp commented code

--- Assets/FlowEnt/Scripts/Runtime/Motions/Tween/Abstract/Values/AbstractGradientMotion.cs
+++ Assets/FlowEnt/Scripts/Runtime/Motions/Tween/Abstract/Values/AbstractGradientMotion.cs
@@ -3,18 +3,18 @@ using UnityEngine;
 
 namespace FriedSynapse.FlowEnt.Motions.Tween.Abstract
 {
-    public abstract class AbstractGradientMotion<TItem> : AbstractValueMotion<TItem, Gradient>
-        where TItem : class
-    {
-        protected AbstractGradientMotion(TItem item, Gradient value) : base(item, value)
-        {
-        }
+    // public abstract class AbstractGradientMotion<TItem> : AbstractValueMotion<TItem, Gradient>
+    //     where TItem : class
+    // {
+    //     protected AbstractGradientMotion(TItem item, Gradient value) : base(item, value)
+    //     {
+    //     }
 
-        protected AbstractGradientMotion(TItem item, Gradient from, Gradient to) : base(item, from, to)
-        {
-        }
+    //     protected AbstractGradientMotion(TItem item, Gradient from, Gradient to) : base(item, from, to)
+    //     {
+    //     }
 
-        protected override Func<Gradient, Gradient, float, Gradient> LerpFunction => Color.LerpUnclamped;
-        protected override Gradient GetTo(Gradient from, Gradient value) => from + value;
-    }
+    //     protected override Func<Gradient, Gradient, float, Gradient> LerpFunction => Color.LerpUnclamped;
+    //     protected override Gradient GetTo(Gradient from, Gradient value) => from + value;
+    // }
 }
