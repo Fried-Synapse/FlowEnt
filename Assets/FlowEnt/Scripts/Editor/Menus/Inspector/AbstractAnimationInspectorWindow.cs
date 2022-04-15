d00d9bf20a198a4a5bb19a20b31c8dc8a2fd4fd4
Daniel Tranca <80676873+danieltranca@users.noreply.github.com>
2022-04-15 17:14:52 +0100

    chore: grouped windows (#95)

--- Assets/FlowEnt/Scripts/Editor/Menus/Inspector/AbstractAnimationInspectorWindow.cs
+++ Assets/FlowEnt/Scripts/Editor/Menus/Inspector/AbstractAnimationInspectorWindow.cs
@@ -1,22 +1,34 @@
 using UnityEditor;
 using UnityEngine;
+using System;
+using System.Linq;
 #if FlowEnt_Debug || (UNITY_EDITOR && FlowEnt_Debug_Editor)
 using FriedSynapse.FlowEnt.Reflection;
 #endif
 
 namespace FriedSynapse.FlowEnt.Editor
 {
-    public abstract class AbstractAnimationInspectorWindow<TWindow, TAnimation> : EditorWindow
+    public abstract class AbstractAnimationInspectorWindow : EditorWindow
+    {
+    }
+
+    public abstract class AbstractAnimationInspectorWindow<TWindow, TAnimation> : AbstractAnimationInspectorWindow
         where TWindow : AbstractAnimationInspectorWindow<TWindow, TAnimation>
         where TAnimation : AbstractAnimation
     {
         private ControllableSection<TAnimation> controllableSection;
         private Vector2 scrollPosition;
         protected TAnimation Animation { get; private set; }
+        private static Type[] types;
 
         public static void Show(TAnimation animation)
         {
-            TWindow window = CreateWindow<TWindow>(animation.ToString());
+            if (types == null)
+            {
+                Type type = typeof(AbstractAnimationInspectorWindow);
+                types = type.Assembly.GetTypes().Where(t => t.IsClass && !t.IsAbstract && t.IsSubclassOf(type)).ToArray();
+            }
+            TWindow window = CreateWindow<TWindow>(animation.ToString(), types);
             window.Animation = animation;
             window.controllableSection = new ControllableSection<TAnimation>(animation);
             window.Show();
@@ -66,9 +78,13 @@ namespace FriedSynapse.FlowEnt.Editor
         {
             FlowEntEditorGUILayout.LabelFieldBold("Stack Trace:");
 #if FlowEnt_Debug || (UNITY_EDITOR && FlowEnt_Debug_Editor)
-            foreach (string line in Animation.GetFieldValue<string>("stackTrace").Split('\n'))
+            string stackTrace = Animation.GetFieldValue<string>("stackTrace");
+            if (!string.IsNullOrEmpty(stackTrace))
             {
-                FlowEntEditorGUILayout.LabelField(line, FlowEntConstants.Orange, GUILayout.Width(position.width - 20f));
+                foreach (string line in stackTrace.Split('\n'))
+                {
+                    FlowEntEditorGUILayout.LabelField(line, FlowEntConstants.Orange, GUILayout.Width(position.width - 20f));
+                }
             }
 #else
             EditorGUILayout.HelpBox("Stack trace only available when debugging enabled. Please enable it in settings.", MessageType.Info);
