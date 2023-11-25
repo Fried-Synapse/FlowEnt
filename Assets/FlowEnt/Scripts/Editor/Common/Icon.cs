5009b97b80e6497b9aecf44ec04b0a13ca4e4db4
Daniel Tranca <danieltrancax@gmail.com>
2023-11-25 23:03:52 +0100

    feat: added easings.net link

--- Assets/FlowEnt/Scripts/Editor/Common/Icon.cs
+++ Assets/FlowEnt/Scripts/Editor/Common/Icon.cs
@@ -7,6 +7,7 @@ namespace FriedSynapse.FlowEnt.Editor
     {
         public static GUIContent Menu = EditorGUIUtility.IconContent("_Menu@2x", "Menu");
         public static GUIContent Select = EditorGUIUtility.IconContent("d_icon dropdown@2x", "Select");
+        public static GUIContent Info = EditorGUIUtility.IconContent("console.infoicon@2x", "Info");
 
         public static GUIStyle Style = new GUIStyle(EditorStyles.miniButton) { padding = new RectOffset(2, 2, 2, 2) };
     }
