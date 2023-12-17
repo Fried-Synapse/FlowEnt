77f440624b09f5e71eb79d5780dffbb9dd6b74cf
Daniel Tranca <danieltrancax@gmail.com>
2023-12-17 22:09:38 +0100

    noob: remove namespace

--- Assets/FlowEnt/Scripts/Editor/Windows/Previewer/PreviewController.cs
+++ Assets/FlowEnt/Scripts/Editor/Windows/Previewer/PreviewController.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using FriedSynapse.FlowEnt.Motions.Abstract;
 using FriedSynapse.FlowEnt.Reflection;
 using UnityEditor;
